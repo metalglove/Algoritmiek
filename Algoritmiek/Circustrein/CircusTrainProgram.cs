f80e0389801fddae7ea8e3968ddee91531c579ea
Manasseh Hofwijks <PS160371@summacollege.nl>
2018-12-10 01:02:42 +0100

    Made Animals and Train readonly in CircusTrainProgram. Changed 50 hard coded Animals to 50 randomly generated Animals. Removed now obsolete tests.

--- Algoritmiek/Circustrein/CircusTrainProgram.cs
+++ Algoritmiek/Circustrein/CircusTrainProgram.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Algoritmiek.Circustrein
 {
@@ -11,14 +12,19 @@ namespace Algoritmiek.Circustrein
     public class CircusTrainProgram : IProgram
     {
         /// <summary>
+        /// Gets the object used for generating random numbers.
+        /// </summary>
+        private readonly static Random _random = new Random();
+
+        /// <summary>
         /// Gets or sets the queue of animals.
         /// </summary>
-        public Queue<Animal> Animals { get; private set; }
+        public Queue<Animal> Animals { get; }
 
         /// <summary>
         /// Gets or sets the train for the circus animals to be put on to.
         /// </summary>
-        public Train Train { get; private set; }
+        public Train Train { get; }
 
         /// <inheritdoc cref="IProgram.Name"/>
         public string Name => GetType().Name;
@@ -30,62 +36,25 @@ namespace Algoritmiek.Circustrein
         public bool HasSetupRan { get; private set; }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="CircusTrainProgram"/> class.
+        /// </summary>
+        public CircusTrainProgram()
+        {
+            Animals = new Queue<Animal>();
+            Train = new Train();
+        }
+
+        /// <summary>
         /// Sets up a queue of 50 Animals.
         /// </summary>
         public void Setup()
         {
-            Animals = new Queue<Animal>();
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Carnivore));
-            Animals.Enqueue(new Animal(size: Size.Small, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Medium, eatingBehaviour: EatingBehaviour.Herbivore));
-            Animals.Enqueue(new Animal(size: Size.Big, eatingBehaviour: EatingBehaviour.Herbivore));
-            Train = new Train();
+            Size[] sizes = (Size[])Enum.GetValues(typeof(Size));
+            EatingBehaviour[] eatingBehaviours = (EatingBehaviour[])Enum.GetValues(typeof(EatingBehaviour));
+
+            for (int i = 0; i < 50; i++)
+                Animals.Enqueue(new Animal(sizes[_random.Next(sizes.Length)], eatingBehaviours[_random.Next(eatingBehaviours.Length)]));
+
             HasSetupRan = true;
         }
 
